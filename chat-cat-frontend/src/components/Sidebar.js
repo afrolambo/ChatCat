23a5557d790d5ce7e732f90c37506eb966a0e5b5
afrolambo <stephennoahgalvan@gmail.com>
2020-12-16 20:53:56 -0500

    Sidebar

---
+++ chat-cat-frontend/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Tab, Nav} from 'react-bootstrap'
+
+const Sidebar = ({id}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Sidebar;
