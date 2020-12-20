c49cdb76d575b38ec63d24b27c65945025819e86
afrolambo <stephennoahgalvan@gmail.com>
2020-12-20 10:30:12 -0500

    open new contact modal

--- chat-cat-frontend/src/components/NewContactModal.js
+++ chat-cat-frontend/src/components/NewContactModal.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import { Modal, Form, Button } from 'react-bootstrap'
 
-const NewContactModal = () => {
+const NewContactModal = ({ closeModal }) => {
+    const idRef = useRef() 
+    const nameRef = useRef()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <Modal.Header closeButton>Create Contact</Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Id</Form.Label>
+                        <Form.Control type="text" ref={idRef} required/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" ref={nameRef} required/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+        </>
     );
 }
 
