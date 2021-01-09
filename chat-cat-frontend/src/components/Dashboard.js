52cf338b31b0d9b9179df3f546a0e1ff5b05c259
afrolambo <stephennoahgalvan@gmail.com>
2021-01-08 23:31:28 -0500

    dashboard set current conversation

--- chat-cat-frontend/src/components/Dashboard.js
+++ chat-cat-frontend/src/components/Dashboard.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import OpenConversation from './OpenConversation'
+import { useConversations } from '../contexts/ConversationsProvider'
 import Welcome from './Welcome'
 
 export default function Dashboard({ id }) {
+    const { selectedConversation } = useConversations()
     return (
         <div>
             <div className="d-flex" style={{ height: '100vh' }}>
                 <Sidebar id={id} />
-                <OpenConversation />
+                { selectedConversation && <OpenConversation /> }
             </div>
         </div>
     )
