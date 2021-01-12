5d7b225f71d63df258dffc9a9dd32e0ec43d27ef
afrolambo <stephennoahgalvan@gmail.com>
2021-01-11 21:46:13 -0500

    create open conversation and form field

--- chat-cat-frontend/src/components/OpenConversation.js
+++ chat-cat-frontend/src/components/OpenConversation.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 export default function OpenConversation() {
+    const [text, setText] = useState('')
     return (
-        <div>
-            
+        <div className="d-flex flex-column flex-grow-1">
+            <div className="flex-grow-1 overflow-auto">
+
+            </div>
+            <Form>
+                <Form.Group className="m-2">
+                    <InputGroup>
+                        <Form.Control 
+                        as="textarea" 
+                        required
+                        value={text}
+                        onChange={e => setText(e.target.value)}
+                        style={{ height: '75px', resize: "none" }}
+                        />
+                        <InputGroup.Append>
+                            <Button type="submit">Send</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Form.Group>
+            </Form>
         </div>
     )
 }
