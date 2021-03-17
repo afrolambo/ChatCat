7ce9becc67648bde1c90926612b663ed9d8dbc66
afrolambo <stephennoahgalvan@gmail.com>
2021-03-16 23:51:54 -0400

    get id to persist

--- chat-cat-frontend/client/src/components/App.js
+++ chat-cat-frontend/client/src/components/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Login from './Login'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 // import Welcome from './Welcome'
 
 
 function App() {
-  const [id, setId] = useState()
+  const [id, setId] = useState('id')
   
   return (
     <>
--- chat-cat-frontend/client/src/hooks/useLocalStorage.js
+++ chat-cat-frontend/client/src/hooks/useLocalStorage.js
@@ -3,15 +3,21 @@ import { useEffect, useState } from 'react'
 const PREFIX = 'ChatCat-'
 
 export default function useLocalStorage(key, initialValue) {
-
     const prefixedKey = PREFIX + key
+    
     const [value, setValue] = useState(() => {
         const jsonValue = localStorage.getItem(prefixedKey)
         if (jsonValue != null) return JSON.parse(jsonValue)
         if (typeOf initialValue === 'function') {
             return initialValue()
         } else {
-            return initialValue 
+            return initialValue() 
         }
     })
+
+    useEffect(() => {
+        localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey, value])
+
+    return [value, setValue]
 }
