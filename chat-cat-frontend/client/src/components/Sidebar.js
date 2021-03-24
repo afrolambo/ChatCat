f8841915a268071384ae38a805e3a81e09590ec6
afrolambo <stephennoahgalvan@gmail.com>
2021-03-23 22:38:49 -0400

    tab container

--- chat-cat-frontend/client/src/components/Sidebar.js
+++ chat-cat-frontend/client/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {Tab, Nav, Button, Modal} from 'react-bootstrap'
+import {Tab, Nav, Button, Modal} from 'react-bootstrap'
 // import Conversations from './Conversations'
 // import Contacts from './Contacts'
 // import NewConversationModal from './NewConversationModal'
@@ -18,38 +18,17 @@ export default function Sidebar({id}) {
     // }
 
     return (
-        // <div style={{width: '250px'}} className="d-flex flex-column">
-        //     <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
-        //         <Nav variant="tabs" className="justify-content-center">
-        //             <Nav.Item>
-        //                 <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
-        //             </Nav.Item>
-        //             <Nav.Item>
-        //                 <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
-        //             </Nav.Item>
-        //         </Nav>
-        //         <Tab.Content className="border-right overflow-auto flex-grow-1">
-        //             <Tab.Pane eventKey={CONVERSATIONS_KEY}>
-        //                 <Conversations />
-        //             </Tab.Pane>
-        //             <Tab.Pane eventKey={CONTACTS_KEY}>
-        //                 <Contacts />
-        //             </Tab.Pane>
-        //         </Tab.Content>
-        //         <div className="p-2 border-top border-right small">
-        //             Your Id: <span className="text-muted">{id}</span>
-        //         </div>
-        //         <Button onClick={() => setModalOpen(true)} className="rounded-0">
-        //             New {conversationsOpen ? 'Conversation' : 'Contact'}
-        //         </Button>
-        //     </Tab.Container>
+        <div style={{width: '250px'}} className="d-flex flex-column">
+           <Tab.Container>
+               
+           </Tab.Container>
 
-        //     <Modal show={modalOpen} onHide={closeModal} >
-        //         {conversationsOpen ? 
-        //             <NewConversationModal closeModal={closeModal}/> : 
-        //             <NewContactModal closeModal={closeModal}/>
-        //             }
-        //     </Modal>
-        // </div>
+            <Modal show={modalOpen} onHide={closeModal} >
+                {conversationsOpen ? 
+                    <NewConversationModal closeModal={closeModal}/> : 
+                    <NewContactModal closeModal={closeModal}/>
+                    }
+            </Modal>
+        </div>
     );
 }
