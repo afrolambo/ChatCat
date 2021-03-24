fe445b3b4a870224f1cf31634db107cbd23cccee
afrolambo <stephennoahgalvan@gmail.com>
2021-03-23 22:36:07 -0400

    create and add sidebar to dashboard

--- chat-cat-frontend/client/src/components/Dashboard.js
+++ chat-cat-frontend/client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from './Sidebar.js'
 
 export default function Dashboard({ id }) {
     return (
-            <div className="d-flex" style={{ height: '100vh' }}>
-                
-            </div>
+            // <div className="d-flex" style={{ height: '100vh' }}>
+                <Sidebar />
+            // </div>
     )
 }
