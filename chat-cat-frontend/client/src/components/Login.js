0d0812a40cce09a4259a8b0f9714d45d24f94a31
afrolambo <stephennoahgalvan@gmail.com>
2021-03-31 23:00:14 -0400

    begin login form, Group Label and Control and useRef for controlling input

--- chat-cat-frontend/client/src/components/Login.js
+++ chat-cat-frontend/client/src/components/Login.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, {useRef} from 'react'
+import { Container, Form } from 'react-bootstrap'
 
 export default function Login() {
     return (
         <Container>
-            Hello Login 
+            <Form>
+                <Form.Group>
+                    <Form.Label>Enter Your ID</Form.Label>
+                    <Form.Control type="text" ref={idRef}></Form.Control>
+                </Form.Group>
+            </Form>
         </Container>
     )
 }
