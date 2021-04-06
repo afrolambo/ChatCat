3c5b828b00f2d168a14ef802f07f4eda87eeccb7
afrolambo <stephennoahgalvan@gmail.com>
2021-04-05 21:34:22 -0400

    styling

--- chat-cat-frontend/client/src/components/App.js
+++ chat-cat-frontend/client/src/components/App.js
@@ -4,10 +4,12 @@ import Login from './Login'
 function App() {
   
   return (
-    <>
+    <div>
       <h2>Chat Cat</h2>
-      <Login />
-    </>
+      <div className="align-items-center d-flex" style={{ height: '100vh' }}>
+        <Login />
+      </div>
+    </div>
   )
 }
 
--- chat-cat-frontend/client/src/components/Login.js
+++ chat-cat-frontend/client/src/components/Login.js
@@ -3,11 +3,12 @@ import { Container, Form } from 'react-bootstrap'
 
 export default function Login() {
     const idRef = useRef()
+
     return (
-        <Container>
-            <Form>
+        <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
+            <Form className="w-100">
                 <Form.Group>
-                    <Form.Label>Enter Your ID</Form.Label>
+                    <Form.Label>Enter Your Id</Form.Label>
                     <Form.Control type="text" ref={idRef} required />
                 </Form.Group>
             </Form>
