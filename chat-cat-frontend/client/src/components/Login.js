73aecde9d56ce77a88010c25520f1acc8f622d08
afrolambo <stephennoahgalvan@gmail.com>
2021-04-06 21:13:20 -0400

    type of button submit

--- chat-cat-frontend/client/src/components/Login.js
+++ chat-cat-frontend/client/src/components/Login.js
@@ -11,7 +11,7 @@ export default function Login() {
                     <Form.Label>Enter Your Id</Form.Label>
                     <Form.Control type="text" ref={idRef} required />
                 </Form.Group>
-                <Button>Login</Button>
+                <Button type="submit">Login</Button>
             </Form>
         </Container>
     )
